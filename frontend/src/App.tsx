1af09ec9e58786fa07cd7072b9a275da3a9ad448
Varun Dubey <varundubey963@gmail.com>
2026-06-27 18:00:37 +0530

    chore(frontend): Initialize frontend project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
